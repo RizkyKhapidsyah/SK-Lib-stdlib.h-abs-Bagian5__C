496531ddf1df44ed230c6fe5688f34bb6cc60865
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-09-28 08:45:36 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-abs-Bagian5__C/Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/*
+	Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+int main() {
+	printf("abs(10) = %d\n", abs(10));
+	printf("abs(-10) = %d\n", abs(-10));
+	printf("abs(50) = %d\n", abs(-50));
+	printf("abs(-50) = %d\n", abs(50));
+
+	_getch();
+	return 0;
+}
